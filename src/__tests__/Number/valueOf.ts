a86970564e5ed0ffd58ff731f44bd01bdeb4ecda
sla <seula00027@gmail.com>
2020-05-19 01:03:04 +0900

    Update valueOf.ts

--- src/__tests__/Number/valueOf.ts
+++ src/__tests__/Number/valueOf.ts
@@ -2,7 +2,11 @@ describe('valueOf', () => {
   // 보통 JavaScript에 의해 내부적으로 호출되고,
   // 웹 코드에서는 명시적으로 호출하지 않는다.
   it('Number객체를 감싼(wrapped) 원시 값을 반환해야 한다.', () => {
-    //const numObj: NumberConstructor = new Number(10);
-    //expect(typeof numObj).toEqual(Object);
+    // typescript-difference-between-string-and-string
+    // https://stackoverflow.com/questions/14727044/typescript-difference-between-string-and-string
+    const numObj: Number = new Number(10);
+    const num: number = numObj.valueOf();
+    expect(typeof new Number(10)).toBe('object');
+    expect(typeof num).toBe('number');
   });
 });
