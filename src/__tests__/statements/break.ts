42f227ee9376fa43590de427d9865d199a53f8d8
sla <seula00027@gmail.com>
2020-05-20 05:03:49 +0900

    Create break.ts

---
+++ src/__tests__/statements/break.ts
@@ -0,0 +1,41 @@
+describe('break', () => {
+  it('반복문, switch문, label문을 종료하고 그 다음문으로 프로그램 제어를 넘긴다.', () => {
+    let i = 0;
+    while (i < 6) {
+      if (i === 3) break;
+      i = i + 1;
+    }
+
+    expect(i).toEqual(3);
+  });
+
+  it('label block과 beack문 함께 사용할 수 있다. break문은 자신이 참조하는 label내에 중첩되어야 한다.', () => {
+    let i = 0;
+    outer_block: {
+      inner_block: {
+        i = 2;
+        break outer_block;
+        i = 3; // 건너뛴다.
+      }
+      i = 4; // 건너뛴다.
+    }
+
+    expect(i).toEqual(2);
+  });
+
+  it('label이 중첩되지 않고 break를 사용한 경우, Syntax Error가 발생한다.', () => {
+    const labels = () => {
+      let i = 1;
+      block_1: {
+        i = 2;
+        // break block2;
+      }
+
+      block_2: {
+        i = 3;
+      }
+    };
+
+    // expect(() => labels()).toThrow(SyntaxError);
+  });
+});
